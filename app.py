cea3c3e787e0ff83d1b67d8761cfcc4cdef775c4
kulkarni15raghav <67050105+kulkarni15raghav@users.noreply.github.com>
2021-07-08 22:42:27 +0530

    dev_commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template,redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField
+from wtforms.validators import InputRequired,Email,Length
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+Bootstrap(app)
+
+app.config['SECRET_KEY']= "Thisissupposedtobesecret!"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///database.db"
+db=SQLAlchemy(app)
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(15),unique=True)
+    password=db.Column(db.String(20))
+    email=db.Column(db.String(30),unique=True)
+
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[InputRequired(),Length(min=4,max=15)])
+    password = PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email',validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired(),Length(min=8,max=80)])
+    username = StringField('username', validators=[InputRequired(),Length(min=4,max=15)])
+    remember = BooleanField('remember me')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.password==form.password.data:
+               return redirect('/dashboard')
+        return "Invalid username/password"
+    return render_template('login.html', form =form )
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    form=RegisterForm()
+    if form.validate_on_submit():
+         new_user=User(username=form.username.data,email=form.email.data,password=form.password.data)
+         db.session.add(new_user)
+         db.session.commit()
+         return "New user has been created!"
+    return render_template('signup.html',form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
